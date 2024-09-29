b751b2e8bd0208727fee3394b2ab1a00b9d39662
JeanCarlos <jmatochepascual@gmail.com>
2024-09-29 08:55:50 -0500

    feat: :sparkles: Crea el componente Header con la estructura visual
    Crea el componente Header para separa la estructura en un componente propio, no contiene logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <>
-      <header className="py-5 header">
-        <div className="container-xl">
-          <div className="row justify-content-center justify-content-md-between">
-            <div className="col-8 col-md-3">
-              <a href="index.html">
-                <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
-              </a>
-            </div>
-            <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-              <div
-                className="carrito"
-              >
-                <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
-
-                <div id="carrito" className="bg-white p-3">
-                  <p className="text-center">El carrito esta vacio</p>
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
-                        </td>
-                        <td>SRV</td>
-                        <td className="fw-bold">
-                          $299
-                        </td>
-                        <td className="flex align-items-start gap-4">
-                          <button
-                            type="button"
-                            className="btn btn-dark"
-                          >
-                            -
-                          </button>
-                          1
-                          <button
-                            type="button"
-                            className="btn btn-dark"
-                          >
-                            +
-                          </button>
-                        </td>
-                        <td>
-                          <button
-                            className="btn btn-danger"
-                            type="button"
-                          >
-                            X
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-                  <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
-                  <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
@@ -79,7 +12,7 @@ function App() {
         <div className="row mt-5">
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
@@ -94,7 +27,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">SRV</h3>
@@ -109,7 +42,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_03.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_03.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Borland</h3>
@@ -124,7 +57,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_04.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_04.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Vai</h3>
@@ -139,7 +72,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_05.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_05.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
@@ -154,7 +87,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_06.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_06.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">White</h3>
@@ -169,7 +102,7 @@ function App() {
 
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_07.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_07.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
@@ -183,7 +116,7 @@ function App() {
           </div>
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_08.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_08.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Dale</h3>
@@ -197,7 +130,7 @@ function App() {
           </div>
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_09.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_09.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
@@ -211,7 +144,7 @@ function App() {
           </div>
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_10.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_10.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
@@ -225,7 +158,7 @@ function App() {
           </div>
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_11.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_11.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Reed</h3>
@@ -239,7 +172,7 @@ function App() {
           </div>
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-              <img className="img-fluid" src="./public/img/guitarra_12.jpg" alt="imagen guitarra" />
+              <img className="img-fluid" src="/img/guitarra_12.jpg" alt="imagen guitarra" />
             </div>
             <div className="col-8">
               <h3 className="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
